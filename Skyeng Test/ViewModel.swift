732e74e7929e0f716ff7abf73433793275649cc4
Georgiy Sabanov <gsabanov@synergy.ru>
2020-07-11 15:37:52 +0300

    Базовая настройка родительских классов. Запуск пустого проекта

---
+++ Skyeng Test/ViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ViewModel.swift
+//  Skyeng Test
+//
+//  Created by Георгий Сабанов on 11.07.2020.
+//  Copyright © 2020 Georgiy Sabanov. All rights reserved.
+//
+
+import Foundation
+protocol ViewModelProtocol: ViewModel {
+    
+}
+class ViewModel: ViewModelProtocol {
+    
+}
