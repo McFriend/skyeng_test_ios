b9612d7aadb56e88f154a2f3e5aa023c13e02d50
Georgiy Sabanov <gsabanov@synergy.ru>
2020-07-11 15:05:54 +0300

    new project

---
+++ Skyeng Test/Skyeng Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skyeng Test
+//
+//  Created by Георгий Сабанов on 11.07.2020.
+//  Copyright © 2020 Georgiy Sabanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
